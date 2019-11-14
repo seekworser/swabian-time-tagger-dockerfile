6cfcd94db5141a3d2abf9b929432c14b8cc7a938
seekworser <seekworser1963@gmail.com>
2019-11-14 14:00:36 +0900

    add requirement installation

---
+++ swabian-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if !(type "brew" > /dev/null 2>&1); then
+    while :
+    do
+        echo "Homebrew have not been installed in your device,"
+        read -p "and will be installed. Proceed? [y/n]: " -n 1 yn
+        case $yn in
+            [Yy]* ) break;;
+            [NnQq]* )
+            echo "\nInstall cancelled."
+            exit;;
+            * ) echo "\nPlease answer y or n.";;
+        esac
+    done
+    echo "Installing Homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Following software will be installed."
+echo "- docker\n- xquartz\n- virtualbox\n- virtualbox-extension-pack"
+
+brew update
+brew cask install docker
+brew cask install xquartz
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
